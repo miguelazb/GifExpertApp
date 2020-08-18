731035aa49ae7e67484741c7802f2402c3b5a61e
Miguel Zamorano <miguelzzam@gmail.com>
2020-08-18 16:08:32 -0700

    added /docs

---
+++ docs/precache-manifest.1482a843b700a5c5a751839214c4e7c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04b1512e7686ca3e19f315b86f19b011",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9aaa7125b4dc55ffa6da",
+    "url": "/static/css/main.e97a7478.chunk.css"
+  },
+  {
+    "revision": "24e715c6aae1d21b0791",
+    "url": "/static/js/2.d36c045a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d36c045a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aaa7125b4dc55ffa6da",
+    "url": "/static/js/main.2162ca65.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
